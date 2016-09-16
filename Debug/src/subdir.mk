0eee6b9695c130f91430dd20a10ed880ed390084
fforcher <francesco.forcher@gmail.com>
2016-09-16 16:35:22 +0200

    Aggiunti i vari script per compilare e selezionare ROOT

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++  "$<" -std=c++0x -DDEBUG -UNDEBUG -O0 -g3 -Wall -c -fmessage-length=0 `~/ROOT/v5-34-36/bin/root-config --cflags` -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@"
+	g++  "$<" -std=c++0x -DDEBUG -O0 -g3 -Wall -c -fmessage-length=0 `root-config --cflags` -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@"
 	@echo 'Finished building: $<'
 	@echo ' '
 
