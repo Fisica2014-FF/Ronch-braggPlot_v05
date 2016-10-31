be49c9156edbe4cdd574448dbaa74f35e8ab858d
fforcher <francesco.forcher@gmail.com>
2016-10-31 15:02:05 +0100

    Riordinati i file, messa una sorta di hello_root
    Test preso da https://github.com/cxx-hep/root-cern/blob/master/test

---
+++ Debug/src/Hello.d
@@ -0,0 +1,183 @@
+src/Hello.o: ../src/Hello.cxx \
+ /home/fforcher/ROOT/v6-06-08/include/TROOT.h \
+ /home/fforcher/ROOT/v6-06-08/include/TDirectory.h \
+ /home/fforcher/ROOT/v6-06-08/include/TNamed.h \
+ /home/fforcher/ROOT/v6-06-08/include/TObject.h \
+ /home/fforcher/ROOT/v6-06-08/include/Rtypes.h \
+ /home/fforcher/ROOT/v6-06-08/include/RtypesCore.h \
+ /home/fforcher/ROOT/v6-06-08/include/RConfig.h \
+ /home/fforcher/ROOT/v6-06-08/include/RVersion.h \
+ /home/fforcher/ROOT/v6-06-08/include/DllImport.h \
+ /home/fforcher/ROOT/v6-06-08/include/Rtypeinfo.h \
+ /home/fforcher/ROOT/v6-06-08/include/snprintf.h \
+ /home/fforcher/ROOT/v6-06-08/include/strlcpy.h \
+ /home/fforcher/ROOT/v6-06-08/include/TGenericClassInfo.h \
+ /home/fforcher/ROOT/v6-06-08/include/TSchemaHelper.h \
+ /home/fforcher/ROOT/v6-06-08/include/TStorage.h \
+ /home/fforcher/ROOT/v6-06-08/include/TVersionCheck.h \
+ /home/fforcher/ROOT/v6-06-08/include/Riosfwd.h \
+ /home/fforcher/ROOT/v6-06-08/include/TBuffer.h \
+ /home/fforcher/ROOT/v6-06-08/include/TString.h \
+ /home/fforcher/ROOT/v6-06-08/include/TMathBase.h \
+ /home/fforcher/ROOT/v6-06-08/include/RStringView.h \
+ /home/fforcher/ROOT/v6-06-08/include/RConfigure.h \
+ /home/fforcher/ROOT/v6-06-08/include/TList.h \
+ /home/fforcher/ROOT/v6-06-08/include/TSeqCollection.h \
+ /home/fforcher/ROOT/v6-06-08/include/TCollection.h \
+ /home/fforcher/ROOT/v6-06-08/include/TIterator.h \
+ /home/fforcher/ROOT/v6-06-08/include/TDatime.h \
+ /home/fforcher/ROOT/v6-06-08/include/TUUID.h \
+ /home/fforcher/ROOT/v6-06-08/include/TSystem.h \
+ /home/fforcher/ROOT/v6-06-08/include/TInetAddress.h \
+ /home/fforcher/ROOT/v6-06-08/include/TTimer.h \
+ /home/fforcher/ROOT/v6-06-08/include/TSysEvtHandler.h \
+ /home/fforcher/ROOT/v6-06-08/include/TQObject.h \
+ /home/fforcher/ROOT/v6-06-08/include/TQObjectEmitVA.h \
+ /home/fforcher/ROOT/v6-06-08/include/TQConnection.h \
+ /home/fforcher/ROOT/v6-06-08/include/Varargs.h \
+ /home/fforcher/ROOT/v6-06-08/include/TInterpreter.h \
+ /home/fforcher/ROOT/v6-06-08/include/TDictionary.h \
+ /home/fforcher/ROOT/v6-06-08/include/ESTLType.h \
+ /home/fforcher/ROOT/v6-06-08/include/TVirtualMutex.h \
+ /home/fforcher/ROOT/v6-06-08/include/TTime.h \
+ /home/fforcher/ROOT/v6-06-08/include/ThreadLocalStorage.h ../src/Hello.h \
+ /home/fforcher/ROOT/v6-06-08/include/TTimer.h \
+ /home/fforcher/ROOT/v6-06-08/include/TCanvas.h \
+ /home/fforcher/ROOT/v6-06-08/include/TPad.h \
+ /home/fforcher/ROOT/v6-06-08/include/TVirtualPad.h \
+ /home/fforcher/ROOT/v6-06-08/include/TAttPad.h \
+ /home/fforcher/ROOT/v6-06-08/include/TVirtualX.h \
+ /home/fforcher/ROOT/v6-06-08/include/TAttLine.h \
+ /home/fforcher/ROOT/v6-06-08/include/TAttFill.h \
+ /home/fforcher/ROOT/v6-06-08/include/TAttText.h \
+ /home/fforcher/ROOT/v6-06-08/include/TAttMarker.h \
+ /home/fforcher/ROOT/v6-06-08/include/GuiTypes.h \
+ /home/fforcher/ROOT/v6-06-08/include/Buttons.h \
+ /home/fforcher/ROOT/v6-06-08/include/TAttBBox2D.h \
+ /home/fforcher/ROOT/v6-06-08/include/TPoint.h \
+ /home/fforcher/ROOT/v6-06-08/include/TAttCanvas.h \
+ /home/fforcher/ROOT/v6-06-08/include/TCanvasImp.h \
+ /home/fforcher/ROOT/v6-06-08/include/TText.h \
+ /home/fforcher/ROOT/v6-06-08/include/TList.h
+
+/home/fforcher/ROOT/v6-06-08/include/TROOT.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TDirectory.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TNamed.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TObject.h:
+
+/home/fforcher/ROOT/v6-06-08/include/Rtypes.h:
+
+/home/fforcher/ROOT/v6-06-08/include/RtypesCore.h:
+
+/home/fforcher/ROOT/v6-06-08/include/RConfig.h:
+
+/home/fforcher/ROOT/v6-06-08/include/RVersion.h:
+
+/home/fforcher/ROOT/v6-06-08/include/DllImport.h:
+
+/home/fforcher/ROOT/v6-06-08/include/Rtypeinfo.h:
+
+/home/fforcher/ROOT/v6-06-08/include/snprintf.h:
+
+/home/fforcher/ROOT/v6-06-08/include/strlcpy.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TGenericClassInfo.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TSchemaHelper.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TStorage.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TVersionCheck.h:
+
+/home/fforcher/ROOT/v6-06-08/include/Riosfwd.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TBuffer.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TString.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TMathBase.h:
+
+/home/fforcher/ROOT/v6-06-08/include/RStringView.h:
+
+/home/fforcher/ROOT/v6-06-08/include/RConfigure.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TList.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TSeqCollection.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TCollection.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TIterator.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TDatime.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TUUID.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TSystem.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TInetAddress.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TTimer.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TSysEvtHandler.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TQObject.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TQObjectEmitVA.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TQConnection.h:
+
+/home/fforcher/ROOT/v6-06-08/include/Varargs.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TInterpreter.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TDictionary.h:
+
+/home/fforcher/ROOT/v6-06-08/include/ESTLType.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TVirtualMutex.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TTime.h:
+
+/home/fforcher/ROOT/v6-06-08/include/ThreadLocalStorage.h:
+
+../src/Hello.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TTimer.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TCanvas.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TPad.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TVirtualPad.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TAttPad.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TVirtualX.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TAttLine.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TAttFill.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TAttText.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TAttMarker.h:
+
+/home/fforcher/ROOT/v6-06-08/include/GuiTypes.h:
+
+/home/fforcher/ROOT/v6-06-08/include/Buttons.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TAttBBox2D.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TPoint.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TAttCanvas.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TCanvasImp.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TText.h:
+
+/home/fforcher/ROOT/v6-06-08/include/TList.h:
--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -3,21 +3,49 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+C_UPPER_SRCS += \
+../src/main_macro.C 
+
+CXX_SRCS += \
+../src/Hello.cxx 
+
 CPP_SRCS += \
-../src/bP_05.cpp 
+../src/main.cpp 
+
+C_UPPER_DEPS += \
+./src/main_macro.d 
+
+CXX_DEPS += \
+./src/Hello.d 
 
 OBJS += \
-./src/bP_05.o 
+./src/Hello.o \
+./src/main.o \
+./src/main_macro.o 
 
 CPP_DEPS += \
-./src/bP_05.d 
+./src/main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cxx
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++  "$<" -std=c++0x -DDEBUG -O0 -g3 -Wall -c -fmessage-length=0 `root-config --cflags` -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++  "$<" -std=c++0x -DDEBUG -O0 -g3 -Wall -c -fmessage-length=0 `root-config --cflags` -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@"
+	g++  "$<" -std=c++0x -DDEBUG -O0 -g3 -Wall -c -fmessage-length=0 `root-config --cflags` -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.C
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++  "$<" -std=c++0x -DDEBUG -O0 -g3 -Wall -c -fmessage-length=0 `root-config --cflags` -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@"
 	@echo 'Finished building: $<'
 	@echo ' '
 
